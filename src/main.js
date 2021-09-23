9429804d3f74f2191e3eb60d39899bbdcf0b2f2a
Jsanderson2005 <joshua@thisisthesandersons.co.uk>
2021-09-23 22:03:37 +0100

    Customised skeleton

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { initExpress } from './libs/express'
 import log from './utils/log'
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
 
 log('info', 'Skeleton', 'Skeleton Loaded')
 
-initExpress()
+const app = express()
+const dev = process.env.NODE_ENV === 'development'
+
+app.use(cors())
+
+app.get('/dist/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../../../ui/dist/bundle.js'))
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../../../ui/src/public/index.html'))
+})
+
+const port = process.env.NODE_ENV === 'development' ? 3001 : 3000
+
+app.listen(port, () => {
+  log('info', 'Skeleton', `http://localhost:${port}`)
+})
+
