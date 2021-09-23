d98d7888334453981fb22d278ae0d9f142ddd52e
Jsanderson2005 <joshua@thisisthesandersons.co.uk>
2021-09-23 22:05:07 +0100

    Customised skeleton

--- src/main.js
+++ src/main.js
@@ -6,19 +6,14 @@ import cors from 'cors'
 log('info', 'Skeleton', 'Skeleton Loaded')
 
 const app = express()
-const dev = process.env.NODE_ENV === 'development'
 
 app.use(cors())
 
-app.get('/dist/bundle.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../../ui/dist/bundle.js'))
+app.get('/', (req, res) => {
+  res.send("Server side!")
 })
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../../../ui/src/public/index.html'))
-})
-
-const port = process.env.NODE_ENV === 'development' ? 3001 : 3000
+var port = 3001;
 
 app.listen(port, () => {
   log('info', 'Skeleton', `http://localhost:${port}`)
