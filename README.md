846573c8754d7846f901efcc0fb9dbcd1ccafc02
Jsanderson2005 <joshua@thisisthesandersons.co.uk>
2021-09-23 22:00:52 +0100

    first commit, skeleton files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Main Radio Website
+
+React source code for the main radio website
+
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { initExpress } from './libs/express'
+import log from './utils/log'
+
+log('info', 'Skeleton', 'Skeleton Loaded')
+
+initExpress()
